3600cd5d645845cc98743aad4206108538f50c64
xiao <xiaohanhao@126.com>
2020-02-12 22:49:54 +0800

    创建

---
+++ Project2/源.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class Preson
+{
+
+public:
+	Preson operator+(Preson& p)
+	{
+		Preson temp;
+		temp.m_a = this->m_a + p.m_a;
+		temp.m_b = this->m_b + p.m_b;
+		return temp;
+	}
+	int m_a;
+	int m_b;
+
+};
+//Preson operator+(Preson& p1, Preson& p2, Preson& p3)
+//{
+//	Preson temp;
+//	temp.m_a = p1.m_a + p2.m_a;
+//	temp.m_b = p1.m_b + p2.m_b;
+//	return temp;
+//}
+void test01()
+{
+	Preson p1;
+	Preson p2;
+	p1.m_a = 10;
+	p1.m_b = 10;
+	p2.m_a = 10;
+	p2.m_b = 10;
+
+	Preson p3 = p1 + p2;
+	cout << "p3.m_a=" << p3.m_a << endl;
+	cout << "p3.m_b=" << p3.m_b << endl;
+}
+
+int main(void)
+{
+	test01();
+
+}
